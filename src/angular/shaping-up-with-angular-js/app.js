e542bc33fb5bb58b4236e3514e7cafef2f4ba4f2
Mattias Kindborg <mattias.kindborg@gmail.com>
2016-02-01 22:48:39 +0100

    Completed chapter 1.

--- src/angular/shaping-up-with-angular-js/app.js
+++ src/angular/shaping-up-with-angular-js/app.js
@@ -2,12 +2,23 @@
     var app = angular.module('store', []);
     
     app.controller('StoreController', function() {
-        this.product = gem;
+        this.products = gems;
     });
     
-    var gem = {
-        name: 'Dodecahedron',
-        price: 2.95,
-        description: '...'
-    };
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: '...',
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: 'Pentagonal Gem',
+            price: 5.95,
+            description: '...',
+            canPurchase: false,
+            soldOut: false
+        }
+    ];
 })();
