01515a17edbee3fe1dbb649254b68b7fd96e79e0
Mattias Kindborg <mattias.kindborg@gmail.com>
2016-02-03 23:20:23 +0100

    Completed chapter 4.

--- src/angular/shaping-up-with-angular-js/app.js
+++ src/angular/shaping-up-with-angular-js/app.js
@@ -4,34 +4,62 @@
     app.controller('StoreController', function(){
         this.products = gems;
     });
-    
-    app.controller('GalleryController', function() {
-        this.current = 0;
-
-        this.setCurrent = function(setCurrent) {
-            this.current = setCurrent ? setCurrent : 0;
-        };
-    });
-    
-    app.controller('TabController', function() {
-        this.tab = 1;
-
-        this.setTab = function(setTab) {
-            this.tab = setTab;
-        };
-
-        this.isSet = function(checkTab) {
-            return this.tab == checkTab;
-        };
-    });
-
-    app.controller('ReviewController', function() {
+        
+     app.controller('ReviewController', function() {
         this.review = {};
     
         this.addReview = function(product) {
             this.review.createdOn = Date.now();
             product.reviews.push(this.review);
             this.review = {};
+        };
+    });
+    
+    app.directive("productGallery", function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-gallery.html',
+            controller: function() {
+                this.current = 0;
+                this.setCurrent = function(imageNumber) {
+                    this.current = imageNumber || 0;
+                };
+            },
+            controllerAs: 'gallery'
+        };
+    });
+
+    
+    app.directive("productTabs", function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-tabs.html',
+            controller: function() {
+                this.tab = 1;
+
+                this.isSet = function(checkTab) {
+                    return this.tab === checkTab;
+                };
+
+                this.setTab = function(setTab) {
+                    this.tab = setTab;
+                };
+            },
+            controllerAs: 'tab'
+        };
+    });
+
+    app.directive('productDescription', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-description.html'
+        };
+    });
+    
+    app.directive("productSpecs", function() {
+        return {
+            restrict: 'A',
+            templateUrl: 'product-specs.html'
         };
     });
 
