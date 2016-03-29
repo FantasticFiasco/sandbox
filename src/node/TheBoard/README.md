d1b70a23e8042c2d0c11334461fe43f46077d442
Mattias Kindborg <mattiask@axis.com>
2016-03-29 16:30:16 +0200

    Start building a web site with Node.js.

---
+++ src/node/TheBoard/README.md
@@ -0,0 +1,3 @@
+﻿# The Board
+
+This code comes from the Pluralsight course [Node.js for .NET Devlopers](https://app.pluralsight.com/library/courses/nodejs-dotnet-developers).
---
+++ src/node/TheBoard/server.js
@@ -0,0 +1,16 @@
+﻿var http = require("http");
+var express = require("express");
+
+var app = express();
+
+app.get("/", function (req, res) {
+    res.send("<html><body><h1>Express</h1></body></html>");
+});
+
+app.get("/api/users", function (req, res) {
+    res.set("Content-Type", "application/json");
+    res.send({ name: "FantasticFiasco", isValid: true, group: "Admin" });
+});
+
+var server = http.createServer(app);
+server.listen(3000);
