3cafc9529c520ff9c6188dc3a9c0d16486098f1f
Mattias Kindborg <mattias.kindborg@gmail.com>
2016-05-07 09:15:33 +0200

    Implement redirect of references from the Markdown.

--- src/node/markdown-proxy/server.js
+++ src/node/markdown-proxy/server.js
@@ -3,35 +3,98 @@
 var app = require('express')();
 var request = require('request');
 var marked = require('marked');
+var url = require('url');
 
-app.get('/', function(req, res) {
-  var url = req.query.url;
-  
-  // Validate that parameter named 'url' exist
-  if (!url) {
-    sendMarkdownError(res, 'Please specify a parameter named `url` that locates the markdown file.');
-    return;
+/**
+ * Express configuration that routes all incoming requests.
+ */
+app.get('/*', function(req, res) {
+  if (isMarkdownRequest(req)) {
+    processMarkdownRequest(req, res);
   }
-  
-  // Create request for reading markdown
-  request(url, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      sendMarkdownSuccess(res, body);
-    }
-    else {
-      sendMarkdownError(res, `Reading markdown failed due to status: __${response.statusMessage}__`);
-    }
-  });
+  else if (isReferenceRequest(req)) {
+    processReferenceRequest(req, res);
+  }
+  else {
+    processInvalidRequest(res);
+  }
 });
 
-function sendMarkdownError(response, errorMessage) {
-  response.send(marked(`# Error \n${errorMessage}`));
+/**
+ * Determines whether specified request is a request for a Markdown file.
+ * @param {Request} req The request to analyze.
+ * @returns {Boolean} true if request is a Markdown request; otherwise false.
+ */
+function isMarkdownRequest(req) {
+  return req.query.url;
 }
 
-function sendMarkdownSuccess(response, message) {
-  response.send(marked(message));
+/**
+ * Processes the specified request and responds with rendered Markdown if successful: otherwise responds with an error message. 
+ * @param  {Request} req The Markdown request to process.
+ * @param  {Response} res The response to finalize.
+ */
+function processMarkdownRequest(req, res) {
+  request(req.query.url, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(marked(body));
+    }
+    else {
+      sendMarkdownError(res, `Reading Markdown failed due to status: ${response.statusMessage}.`);
+    }
+  });
 }
 
+/**
+ * Determines whether specified request is a request for a reference.
+ * @param {Request} req The request to analyze.
+ * @returns {Boolean} true if request is a reference request; otherwise false.
+ */
+function isReferenceRequest(req) {
+  return req.header('Referer');
+}
+
+/**
+ * Processes the specified request and responds with redirect if successful: otherwise responds with an error message. 
+ * @param  {Request} req The Markdown request to process.
+ * @param  {Response} res The response to finalize.
+ */
+function processReferenceRequest(req, res) {
+  var referer = url.parse(req.header('Referer'));
+  
+  // Regex for parsing e.g.
+  //    'http://www.markdown.com'
+  // from
+  //    'http://<host>/?url=http://www.markdown.com/markdown_sample.md'
+  var baseUrl = /\/\?url=(.+)(?:\/.+\.md)$/gi.exec(referer.path)[1];
+  
+  if (baseUrl) {
+    var resourceUrl = baseUrl + req.path;
+    res.redirect(resourceUrl);
+  }
+  else {
+    sendMarkdownError(res, `Unable to parse the location of the Markdown from ${referer.path}.`);
+  }
+}
+
+/**
+ * Finalizes the specified response by sending an error message that displays the usage of the appliction. 
+ * @param  {Response} res The response to finalize.
+ */
+function processInvalidRequest(res) {
+  sendMarkdownError(res, 'Invalid request, the request must have the parameter [/?url=[url]](/?url=[url]) where `[url]` is the location of the markdown.');
+}
+
+/**
+ * Finalizes the specified response with the specified error message.
+ */
+function sendMarkdownError(res, errorMessage) {
+  res.send(marked(`# Error \n${errorMessage}`));
+}
+
+/**
+ * Start the web server.
+ */
 app.listen(3000, function () {
   console.log('Starting to listen for requests on port 3000');
 });
