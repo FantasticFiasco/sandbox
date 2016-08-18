9fd52285022c49dae21358e6e0a6cfc2364cd2e5
FantasticFiasco <mattias.kindborg@gmail.com>
2016-08-18 16:32:50 +0200

    Add text and button
    Add a simple text that will change color each time
    the button is clicked.

--- src/angular-2/quickstart/app/app.component.ts
+++ src/angular-2/quickstart/app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+	title = 'Customer App';
+	name = 'Ward';
+	wardsColor = 'blue'
+
+	changeSuitColor() {
+		this.wardsColor = this.wardsColor === 'blue' ? 'red' : 'blue';
+	}
+ }
