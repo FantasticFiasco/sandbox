5e6ce91dcf1f7bdc8f3b1042b8e267e15a159f1d
FantasticFiasco <mattias.kindborg@gmail.com>
2016-08-24 11:32:02 +0200

    Extract customer component

--- src/angular-2/quickstart/app/app.component.ts
+++ src/angular-2/quickstart/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
+import { CustomerComponent } from './customer/customer.component'
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.component.html'
+    templateUrl: 'app/app.component.html',
+	directives: [CustomerComponent]
 })
 export class AppComponent {
 	title = 'Customer App';
 	name = 'Ward';
-	wardsColor = 'blue';
+	wardsColor = 'green';
+	customers = [
+		{ id: 1, name: 'Ward' },
+		{ id: 2, name: 'Kevin' },
+		{ id: 3, name: 'Eric' },
+		{ id: 4, name: 'Sally' },
+		{ id: 5, name: 'Emmet' },
+	];
 
 	changeSuitColor() {
-		this.wardsColor = this.wardsColor === 'blue' ? 'red' : 'blue';
+		this.wardsColor = this.wardsColor === 'green' ? 'red' : 'green';
 	}
  }
