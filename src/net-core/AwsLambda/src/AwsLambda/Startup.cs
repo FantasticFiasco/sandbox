1c606a6fa876c475548763d9e0e89c43c384cc2d
FantasticFiasco <mattias.kindborg@gmail.com>
2016-12-05 23:35:32 +0100

    Add AWS Lambda written in C#

---
+++ src/net-core/AwsLambda/src/AwsLambda/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace AwsLambda
+{
+    public class Startup
+    {
+        public Stream Run()
+        {
+            var buffer = Encoding.UTF8.GetBytes($"{DateTime.Now} - Hello World!");
+            return new MemoryStream(buffer);
+        }
+    }
+}
