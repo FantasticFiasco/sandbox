bb6d0f46ff59c9da5e9f39bbbaaab092fcc2cdd8
FantasticFiasco <mattias.kindborg@gmail.com>
2018-03-26 07:25:20 +0200

    wip

--- src/load-tests/artillery/server.js
+++ src/load-tests/artillery/server.js
@@ -6,10 +6,12 @@ app.use(express.json());
 app.post('/search', (req, res) => {
     console.log(`search - keyword '${req.body.kw}'`);
 
-    res.status(200).json([
-        { id: 1, name: 'blue shirt' },
-        { id: 2, name: 'red shirt' }
-    ]);
+    res.status(200).json({
+        results: [
+            { id: 1, name: 'blue shirt' },
+            { id: 2, name: 'red shirt' }
+        ]
+    });
 });
 
 app.get('/details/:id', (req, res) => {
