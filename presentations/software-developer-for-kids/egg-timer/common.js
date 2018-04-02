a997c0a58d9c6d84afb4de94fb4f355e05d79ee9
FantasticFiasco <mattias.kindborg@gmail.com>
2018-04-02 22:18:29 +0200

    chore: egg-timer presentation demo

---
+++ presentations/software-developer-for-kids/egg-timer/common.js
@@ -0,0 +1,56 @@
+const imageToAscii = require('image-to-ascii');
+var player = require('play-sound')(opts = {});
+
+var antal_sekunder = 5;
+
+function skriv_ut(text) {
+    console.log(text);
+}
+
+function skriv_ut_ägg_klocka() {
+    imageToAscii('./egg-timer.jpg', { size: { height: 20 }, bg: true }, (err, converted) => {
+        console.log(converted);
+    });
+}
+
+function gör_så_länge(condition, callback, endCallback) {
+    const handle = setInterval(
+        () => {
+            if (condition()) {
+                callback();
+            } else {
+                clearInterval(handle);
+
+                if(endCallback) {
+                    endCallback();
+                }
+            }
+        },
+        1000);
+}
+
+function antal_sekunder_är_större_än_0() {
+    return antal_sekunder > 0;
+}
+
+function skriv_ut_antal_sekunder_kvar() {
+    console.log(`${antal_sekunder} sekunder kvar`);
+}
+
+function räkna_ner_antal_sekunder() {
+    antal_sekunder--;
+}
+
+function spela_ljud() {
+    player.play('./rooster.wav', { mplayer: ['-ao', 'alsa'] });
+}
+
+module.exports = {
+    antal_sekunder,
+    skriv_ut_ägg_klocka,
+    gör_så_länge,
+    antal_sekunder_är_större_än_0,
+    skriv_ut_antal_sekunder_kvar,
+    räkna_ner_antal_sekunder,
+    spela_ljud
+};
---
+++ presentations/software-developer-for-kids/egg-timer/index.js
@@ -0,0 +1,11 @@
+require('underscore')(global).extend(require('./common'))
+
+// Funktioner som vi kan använda:
+//   1. skriv_ut_ägg_klocka
+//   2. gör_så_länge
+//   3. antal_sekunder_är_större_än_0
+//   4. skriv_ut_antal_sekunder_kvar
+//   5. räkna_ner_antal_sekunder
+//   6. spela_ljud
+
+antal_sekunder = 5;
