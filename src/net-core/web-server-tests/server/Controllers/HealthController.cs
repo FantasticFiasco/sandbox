9516176f0e024ce27c1a9bcaa3fc72fbc6cec6c0
FantasticFiasco <mattias.kindborg@gmail.com>
2018-04-18 22:50:18 +0200

    wip

---
+++ src/net-core/web-server-tests/server/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return NoContent();
+        }
+    }
+}
--- src/net-core/web-server-tests/server/Startup.cs
+++ src/net-core/web-server-tests/server/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Server
 {
@@ -18,6 +19,12 @@ namespace Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // Configure OpenAPI
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info { Title = "Web Server Tests API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +36,13 @@ namespace Server
             }
 
             app.UseMvc();
+
+            // Configure OpenAPI
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web Server Tests API");
+            });
         }
     }
 }
