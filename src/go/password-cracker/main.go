952c05e6b488571e087e0959cd42966b741ce6c4
FantasticFiasco <mattias.kindborg@gmail.com>
2020-10-27 10:03:54 +0100

    wip

--- src/go/password-cracker/main.go
+++ src/go/password-cracker/main.go
@@ -3,18 +3,12 @@ package main
 import (
 	"crypto/md5"
 	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/url"
+	"passwordcracker/logger"
 	"strings"
-)
-
-const (
-	address  = "192.168.1.227"
-	port     = 80
-	username = "test"
-	password = "abc123!#"
 )
 
 func check(err error) {
@@ -24,31 +18,42 @@ func check(err error) {
 }
 
 func main() {
-	u, err := url.Parse(fmt.Sprintf("http://%s:%d/axis-cgi/param.cgi?action=list&group=Brand.Brand", address, port))
-	check(err)
+	address := flag.String("address", "192.168.0.90", "The network IP address")
+	port := flag.Uint("port", 80, "The network port")
+	username := flag.String("username", "root", "The username used when accessing the device")
 
-	a := probeAuthentication(u.String())
+	flag.Parse()
 
-	nc := "00000001"
-	cnonce := "0a4f113b"
+	logger.Infoln("------------------------------")
+	logger.Infof("Target:   %s:%d\n", *address, *port)
+	logger.Infof("Username: %s\n", *username)
+	logger.Infoln("------------------------------")
 
-	ha1 := hash(fmt.Sprintf("%s:%s:%s", username, a.realm, password))
-	ha2 := hash(fmt.Sprintf("%s:%s", http.MethodGet, u.Path+"?"+u.RawQuery))
-	response := hash(fmt.Sprintf("%s:%s:%s:%s:%s:%s", ha1, a.nonce, nc, cnonce, a.qop, ha2))
-
-	c := http.Client{}
-	req := http.Request{
-		Method: http.MethodGet,
-		URL:    u,
-		Header: make(http.Header),
-	}
-	req.Header.Add(
-		"Authorization",
-		fmt.Sprintf("Digest username=\"%s\", realm=\"%s\", nonce=\"%s\", uri=\"%s\", qop=auth, nc=%s, cnonce=\"%s\", response=\"%s\"", username, a.realm, a.nonce, u.Path+"?"+u.RawQuery, nc, cnonce, response))
-	res, err := c.Do(&req)
-	check(err)
-	defer res.Body.Close()
-	printResponse(res)
+	//u, err := url.Parse(fmt.Sprintf("http://%s:%d/axis-cgi/param.cgi?action=list&group=Brand.Brand", address, port))
+	//check(err)
+	//
+	//a := probeAuthentication(u.String())
+	//
+	//nc := "00000001"
+	//cnonce := "0a4f113b"
+	//
+	//ha1 := hash(fmt.Sprintf("%s:%s:%s", username, a.realm, password))
+	//ha2 := hash(fmt.Sprintf("%s:%s", http.MethodGet, u.Path+"?"+u.RawQuery))
+	//response := hash(fmt.Sprintf("%s:%s:%s:%s:%s:%s", ha1, a.nonce, nc, cnonce, a.qop, ha2))
+	//
+	//c := http.Client{}
+	//req := http.Request{
+	//	Method: http.MethodGet,
+	//	URL:    u,
+	//	Header: make(http.Header),
+	//}
+	//req.Header.Add(
+	//	"Authorization",
+	//	fmt.Sprintf("Digest username=\"%s\", realm=\"%s\", nonce=\"%s\", uri=\"%s\", qop=auth, nc=%s, cnonce=\"%s\", response=\"%s\"", username, a.realm, a.nonce, u.Path+"?"+u.RawQuery, nc, cnonce, response))
+	//res, err := c.Do(&req)
+	//check(err)
+	//defer res.Body.Close()
+	//printResponse(res)
 }
 
 type authentication struct {
